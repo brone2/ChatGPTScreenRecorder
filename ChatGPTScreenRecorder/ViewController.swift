77cea22d56020156768bedf25c28ba3ecaf3e732
NB <bronfinn@gmail.com>
2025-02-15 10:05:03 -0500

    Initial Commit

---
+++ ChatGPTScreenRecorder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatGPTScreenRecorder
+//
+//  Created by Neil Bronfin on 2/15/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ChatGPTScreenRecorderTests/ChatGPTScreenRecorderTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTScreenRecorderTests.swift
+//  ChatGPTScreenRecorderTests
+//
+//  Created by Neil Bronfin on 2/15/25.
+//
+
+import Testing
+@testable import ChatGPTScreenRecorder
+
+struct ChatGPTScreenRecorderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
